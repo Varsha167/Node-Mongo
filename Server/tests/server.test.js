244f52b0895c8729f420150c61f8cac0b46aaa0b
Varsha167 <varsha167@gmail.com>
2018-02-07 08:36:08 +0530

    add get todo test

--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -5,33 +5,45 @@ const{app} = require('./../server') //extracting it from module exports
 const{Todo} = require('./../models/todo')
 
 //run this peiece of code before every test case
+const todos = [{
+  text: "todo test 1"
+}, {
+  text: "todo test 2"
+}]
+
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
-done()
-  })
+return Todo.insertMany(todos)
+}).then(()=> done())
 })
+
+  // beforeEach((done)=>{
+  //   Todo.remove({}).then(()=> done())
+  // })
 
 describe('POST /todos' ,()=>{
 it('should create a new todo' , (done)=>{
-  var todoText = 'test todo app'
+  var text1 = 'test todo app'
 
   request(app)
   .post('/todos')
   .send({   // can be written as .send({text})
-    text : todoText
+    text:text1
   })
   .expect(200)
   .expect((res)=>{ //custom expect
-    expect(res.body.text).toBe(todoText)
+    expect(res.body.text).toBe(text1)
   })
   .end((err,res)=>{ //check what got stored in mongo db collection
     if(err) {
     return done(err)
   }
 
-Todo.find().then((todos)=>{ //make a request to the database fetching all todos verifying that our one todo is added
+Todo.find({
+  text : text1
+}).then((todos)=>{ //make a request to the database fetching all todos verifying that our one todo is added
   expect (todos.length).toBe(1)
-  expect(todos[0].text).toBe(todoText)
+  expect(todos[0].text).toBe(text1)
   console.log(todos)
   done()
 }).catch((e)=>done(e)) //why do we need catch here? because otherwise this testcase will pass anyhow. Why? see below
@@ -48,13 +60,27 @@ Todo.find().then((todos)=>{ //make a request to the database fetching all todos 
         return done(err)
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0)
+        expect(todos.length).toBe(2)
         done()
       }).catch((e)=>{
         done(e)
       })
     })
  })
+})
+
+describe('GET Todos',()=>{
+
+it('should get the todo list', (done)=>{
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.content.length).toBe(2)
+  })
+  .end(done)
+})
+
 })
 //
 //
